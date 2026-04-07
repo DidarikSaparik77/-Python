a57de4ed8205dec8353b41f616912d38aecca4b0
DidarikSaparik77 <saparovdidar13@gmail.com>
2026-04-07 13:20:42 +0300

    Update пересчёт величины.py

--- практическая 4/пересчёт величины.py
+++ практическая 4/пересчёт величины.py
@@ -1,5 +1,5 @@
-name1 = int(input())
-print(name1, 'мин - это', name1 // 60, 'час', name1 - (name1 // 60 * 60), 'минут.')
+minutes = int(input())
+print(minutes, 'мин - это', minutes // 60, 'час', minutes - (minutes // 60 * 60), 'минут.')
 
 
 
