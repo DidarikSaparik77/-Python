fc96f0350b151ef4117bfb9d4d151cb1c2671832
DidarikSaparik77 <saparovdidar13@gmail.com>
2026-04-07 13:24:23 +0300

    Update тригонометрическое выражение.py

--- практическая 4/тригонометрическое выражение.py
+++ практическая 4/тригонометрическое выражение.py
@@ -1,8 +1,8 @@
 x_gradus = float(input())
 from math import *
-r = x_gradus * pi / 180
-x = sin(r) + cos(r) + (tan(r) ** 2)
-print(x)
+x_radians = x_gradus * pi / 180
+x_trigono = sin(x_radians) + cos(x_radians) + (tan(x_radians) ** 2)
+print(x_trigono)
 
 
 
