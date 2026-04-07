3ad3cb23909747b2e0cc6960372fe654fb20853d
DidarikSaparik77 <saparovdidar13@gmail.com>
2026-04-07 13:49:17 +0300

    Update евклидово расстояние.py

--- практическая 4/евклидово расстояние.py
+++ практическая 4/евклидово расстояние.py
@@ -1,10 +1,9 @@
 from math import *
 
-x1, y1, x2, y2 = float(input()), float(input()), float(input()), float(input())
+x1, y1, x2, y2 = map(float, input('введите координаты через пробел:').split())
 
 evklidovo_rastoyanie = sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2))
 print(evklidovo_rastoyanie)
-
 
 
 
